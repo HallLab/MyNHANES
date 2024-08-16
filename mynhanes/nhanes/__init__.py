9bd3b6baee6c0886c4a59d7e390cef667c7746b0
Andre <ricoandre@hotmail.com>
2024-08-16 07:43:36 -0400

    drop MVP version

--- mynhanes/nhanes/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'nhanes.apps.NhanesConfig'
--- mynhanes/nhanes/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class NhanesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nhanes'
-
-    def ready(self):
-        import nhanes.signals  # noqa F401
